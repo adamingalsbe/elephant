99fa577bf8bd60bee0df8161fc0f479ff9526e44
Adam Ingalsbe <adamingalsbe@gmail.com>
2013-05-09 12:44:47 -0500

    initial app

---
+++ elephant/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elephant::Application.config.secret_token = 'ca0c1df01ca515e3a3e243fe84ec9ef1c9080fb2d9d845f056dfc04a4487825ecec5818b501312eb8d515aa73334cd64695868f11873bf44dfeef8ed06821382'
