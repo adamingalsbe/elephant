7bbe671d8cdd90662dcf618a5ed0dbc4207490fb
Adam Ingalsbe <adamingalsbe@gmail.com>
2013-05-13 21:30:47 -0500

    Improved the User model with secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,16 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :email_address
+  attr_accessible :first_name, :last_name, :email_address, :password, :password_confirmation
+  has_secure_password
 
+  before_save { |user| user.email = email.downcase }
   validates :first_name, presence: true, length: {minimum: 2, maximum: 50}
   validates :last_name, presence: true, length: {minimum: 2, maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email_address, presence: true, format: { with: VALID_EMAIL_REGEX}
+  validates :email_address, presence: true,
+                  format: { with: VALID_EMAIL_REGEX},
+                  uniqueness: {case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6}
+  validates :password_confirmation, presence: true
 end
