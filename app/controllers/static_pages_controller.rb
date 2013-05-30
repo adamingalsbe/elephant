a9c94f2868a492dcad9fe9a2dfc8167b9f4d4a98
Adam Ingalsbe <adamingalsbe@gmail.com>
2013-05-30 16:33:59 -0500

    fixed css link discoloring

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    if session["user_id"].present?
+    redirect_to "/users/#{session["user_id"]}", notice: "Welcome back!"
+    end
   end
 
   def about
