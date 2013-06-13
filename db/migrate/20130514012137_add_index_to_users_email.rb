5af7a4ec2f692b2fef631816c142c7c1783c8298
david frucher <dfrucher2014@kellogg.northwestern.edu>
2013-06-13 08:30:46 -0500

    Checked and cleaned [DF]

--- db/migrate/20130514012137_add_index_to_users_email.rb
+++ db/migrate/20130514012137_add_index_to_users_email.rb
@@ -1,5 +1,14 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration
   def change
+  	class CreateUsers < ActiveRecord::Migration
+create_table :users do |t|
+  t.string :first_name
+  t.string :last_name
+  t.string :email_address
+  t.timestamps
+end
+end
+
     add_index :users, :email_address, unique: true
   end
 end
